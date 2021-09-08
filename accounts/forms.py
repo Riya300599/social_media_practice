3727cce86b66578a259f0ac1d0d780f4ad3600ec
manojchavan139 <chavan.manoj139@gmail.com>
2021-09-08 18:52:58 +0530

    first commit

---
+++ accounts/forms.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
+from . import models
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = get_user_model()
+        fields = ('username', 'email', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # custom label
+        self.fields['username'].label = 'Name'
+        self.fields['email'].label = 'Email Address'
---
+++ accounts/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from . import forms
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
+
+# Create your views here.
+class SignUp(CreateView):
+    form_class = forms.CreateUserForm
+    success_url = reverse_lazy('login')
+    template_name = 'accounts/signup.html'
